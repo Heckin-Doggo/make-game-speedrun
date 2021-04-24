2ac8da662df3d2cafa28ebec814dec3dcb86ee1a
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-04-24 17:47:04 -0400

    Flashlight assets added

--- deepdivegame/Globals.gd
+++ deepdivegame/Globals.gd
@@ -6,3 +6,7 @@ var player = {
 	"oxygen": 68,  # this is equal to the hieght in pixels of the o2 bar.
 	"depth": 0  # this is equal to the depth of the player in pixel
 }
+
+var powerups = {
+	"flashlight": 0
+}
