e6ed40dc0700319db60b4a7a4bef6cf5cfe500e0
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-04-24 18:21:57 -0400

    flashlight indicator

--- deepdivegame/levels/HUD2.gd
+++ deepdivegame/levels/HUD2.gd
@@ -6,10 +6,22 @@ onready var globals = get_node("/root/Globals")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+#	for i in range(7,-1,-1):
+#		print(i)
+#		globals.powerups["flashlight"] = i
+#		yield(get_tree().create_timer(1), "timeout")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$OxygenDisplay/FillBar.rect_size.y = globals.player["oxygen"]
 	$DepthGauge/Label.text = str(floor(globals.player["depth"]/10))
+	
+	var flashlight_power = globals.powerups["flashlight"]
+	if flashlight_power > 0:
+		print("power: ", flashlight_power)
+		$FlashlightIndicator.visible = true
+		$FlashlightIndicator/Batteries.rect_size.y = 8 * globals.powerups["flashlight"]
+	else:
+		$FlashlightIndicator.visible = false
