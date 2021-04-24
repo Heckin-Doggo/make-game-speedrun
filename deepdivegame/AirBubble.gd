1882ecbc79d8ae640555910914a4dbdd87873d5c
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-04-24 16:38:07 -0400

    bubble functionality

--- deepdivegame/AirBubble.gd
+++ deepdivegame/AirBubble.gd
@@ -3,18 +3,24 @@ extends RigidBody2D
 # globals
 onready var globals = get_node("/root/Globals")
 
+# packed scenes
+const Bubble = preload("res://scenes/Bubble.tscn")
+
 # Declare member variables here
-export var float_speed = 7
+export var float_speed = 32
 export var amplifier = 10
 export var period = 2  # this really shouldnt go above 2. use amplifier instead.
 
+export var oxygen_boost = 20
+
 # var anim_steps = [-2,-1,0,1,2,1,0,-1]
 var time_since_last_cycle = 0
+var debounce = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$HitBox.connect("body_entered", self, "_on_bubble_pop")
 
 
 func _physics_process(delta):
@@ -27,6 +33,17 @@ func _physics_process(delta):
 	linear_velocity.y = -float_speed
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_bubble_pop(body):
+	print("Collided with ", body.name)
+	if not debounce:
+		debounce = true
+		globals.player["oxygen"] = clamp(globals.player["oxygen"]+oxygen_boost, 20, 68)  # 68 is o2 tank size
+		
+		# bubble effect
+		for i in range(5):
+			var bub = Bubble.instance()
+			bub.update_pos(Vector2($Sprite.position.x, $Sprite.position.y-9))  # this makes it local
+			add_child(bub)
+			yield(get_tree().create_timer(0.01),"timeout")  # wait
+		
+		queue_free()
