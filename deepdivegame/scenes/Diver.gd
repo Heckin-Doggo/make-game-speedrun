f5988d8867a6251727709bf3800ef96520060efb
carsonburley <cadetburley@gmail.com>
2021-04-23 22:42:52 -0400

    added better diver movement (still broke though)

--- deepdivegame/scenes/Diver.gd
+++ deepdivegame/scenes/Diver.gd
@@ -2,8 +2,11 @@ extends KinematicBody2D
 
 
 #variables
-var movespeed_x = 10
-var movespeed_y = 8
+export var maxspeed = 80
+export var acceleration = 500
+export var friction = 500
+var input_vector = Vector2.ZERO
+var sink_speed = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,11 +16,22 @@ func _ready():
 # called every physics frame, which is before each drawn frame
 func _physics_process(delta):
 	var velocity = Vector2()
-	var sink = Vector2(0,3)  # sinking vector
 	
-	velocity.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * movespeed_x
-	velocity.y = (Input.get_action_strength("ui.down") - Input.get_action_strength("ui_up")) * movespeed_y
+	input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
+	input_vector.y = (Input.get_action_strength("ui.down") - Input.get_action_strength("ui_up"))
 	
-	var move_vector = (velocity + sink)
 	
-	velocity = move_and_slide(move_vector, Vector2(0,0), false, 4, PI/4, false)
+	input_vector = input_vector.normalized()
+	
+	#adds the sink speed
+	input_vector.y = input_vector.y + sink_speed
+	
+	if(input_vector != Vector2.ZERO):
+		#moves velocity towards the input
+		velocity = velocity.move_toward(input_vector * maxspeed, acceleration * delta)
+		
+	else:
+		#moves velocity toward sink speed
+		velocity = velocity.move_toward(Vector2(0, sink_speed), friction * delta)
+	
+	velocity = move_and_slide(velocity)
