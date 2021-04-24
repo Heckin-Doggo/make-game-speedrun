baf148161aa4116cc56a924042555736c87bdce9
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-04-23 23:01:20 -0400

    undid movement lol

--- deepdivegame/scenes/Diver.gd
+++ deepdivegame/scenes/Diver.gd
@@ -2,11 +2,8 @@ extends KinematicBody2D
 
 
 #variables
-export var maxspeed = 80
-export var acceleration = 500
-export var friction = 500
-var input_vector = Vector2.ZERO
-var sink_speed = 0.1
+export var movespeed_x = 20
+export var movespeed_y = 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,22 +13,11 @@ func _ready():
 # called every physics frame, which is before each drawn frame
 func _physics_process(delta):
 	var velocity = Vector2()
+	var sink = Vector2(0,3)  # sinking vector
 	
-	input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
-	input_vector.y = (Input.get_action_strength("ui.down") - Input.get_action_strength("ui_up"))
+	velocity.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * movespeed_x
+	velocity.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) * movespeed_y
 	
+	var move_vector = (velocity + sink)
 	
-	input_vector = input_vector.normalized()
-	
-	#adds the sink speed
-	input_vector.y = input_vector.y + sink_speed
-	
-	if(input_vector != Vector2.ZERO):
-		#moves velocity towards the input
-		velocity = velocity.move_toward(input_vector * maxspeed, acceleration * delta)
-		
-	else:
-		#moves velocity toward sink speed
-		velocity = velocity.move_toward(Vector2(0, sink_speed), friction * delta)
-	
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(move_vector)
