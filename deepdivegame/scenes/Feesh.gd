0e9d88779b5dc0ad2cc97045ea7bca021cb1644a
carsonburley <cadetburley@gmail.com>
2021-04-24 00:02:00 -0400

    feesh

--- deepdivegame/scenes/Feesh.gd
+++ deepdivegame/scenes/Feesh.gd
@@ -1,12 +1,28 @@
 extends KinematicBody2D
 
-var direction = Vector2.ZERO
-var speed = 20
+var screen_size = get_viewport_rect().size
+var direction
+var speed = 20.0
 var velocity = Vector2.ZERO
 
 func _ready():
-	velocity = direction * speed
+	screen_size = get_viewport_rect().size
+	velocity = Vector2.ZERO
+	direction = Vector2.ZERO
 
 func _physics_process(delta):
-	move_and_slide(direction)
+	move_and_slide(velocity)
 
+func init(spawn_position):
+	
+	velocity = spawn_position.x
+	position.y = spawn_position.y
+	
+	if(spawn_position.x < 0):
+		position.x = 0
+		velocity.x = 1 * speed
+		
+	else:
+		position.x = screen_size.x
+		velocity.x = -1 * speed
+	print("feesh spawned at: ", position)
