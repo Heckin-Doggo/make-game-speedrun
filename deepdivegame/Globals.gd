84d3e27a56e29331e8577dbb693641de4c203736
carsonburley <cadetburley@gmail.com>
2021-04-25 22:42:25 -0400

    added a comma in globals

--- deepdivegame/Globals.gd
+++ deepdivegame/Globals.gd
@@ -4,7 +4,7 @@ extends Node
 # global variables to be declared here
 var player = {
 	"oxygen": 68,  # this is equal to the hieght in pixels of the o2 bar.
-	"depth": 0  # this is equal to the depth of the player in pixel
+	"depth": 0,  # this is equal to the depth of the player in pixel
 	"alive": true
 }
 
