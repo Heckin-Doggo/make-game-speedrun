68745796a198ca8194618c04c6443d69e02df2bd
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-04-26 17:58:32 -0400

    title screen animation

--- deepdivegame/levels/TitleScreen.gd
+++ deepdivegame/levels/TitleScreen.gd
@@ -7,6 +7,9 @@ var ticker = 0.0
 var boat_y = 1
 var delay = 60.0/70.0
 
+onready var Diver = $Boat/Diver
+onready var DiverAnim = $Boat/Diver/Anim
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,5 +29,23 @@ func _process(delta):
 func start_game():
 	print("Game starting!")
 	started = true
+	
+	#animation
+	
+	DiverAnim.animation = "jump"  # ready for jump
+	yield(get_tree().create_timer(0.5),"timeout")
+	DiverAnim.frame = 1  # in the air
+	# enable physics
+	Diver.gravity_scale = .5
+	Diver.linear_velocity = Vector2(5,-45)
+	
+	yield(get_tree().create_timer(1),"timeout")
+	DiverAnim.frame = 2  # diving
+	yield(get_tree().create_timer(1),"timeout")
+	
+	$Splash.play()
+	yield($Splash, "finished")
+	
+	
 	# yield(get_tree().create_timer(3), "timeout")
 	get_tree().change_scene("res://levels/SpawnTestLevel.tscn")
